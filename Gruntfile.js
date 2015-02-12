cee8721c44a18a7ee9c7aa5346f8450ad1312631
Ari Scott <ariscott12@gmail.com>
2015-02-12 06:20:05 -0700

    added gruntfile for live reload and sass compiling

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+    //  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    grunt.initConfig({
+        sass: {
+            dist: {
+
+                files: {
+                    'css/main.css': 'sass/main.scss'
+                }
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    livereload: true
+
+                }
+            }
+        },
+        watch: {
+            sass: {
+                files: [
+                    'sass/*.scss'
+                ],
+                tasks: 'sass'
+            },
+            livereload: {
+                // Here we watch the files the sass task will compile to
+                // These files are sent to the live reload server after sass compiles to them
+                options: {
+                    livereload: true
+                },
+                files: ['css/main.css', 'index.html'],
+            },
+        }
+    });
+
+    //Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+    //Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['connect', 'watch']);
+
+
+
+};
